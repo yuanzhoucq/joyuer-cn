1ceacb9d2c8ad5a12bb8d96fe4c37951d1133b6a
yuanzhoucq <yuanzhou.cq@gmail.com>
2018-07-13 20:24:53 +0200

    link my apps

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,5 +6,10 @@ export default ({data}) => (
       About
     </h1>
     <p>I’m good, people like me!</p>
+
+    <h2>My Apps</h2>
+
+    <h3><a href="https://www.joyuer.cn/app/template-lettre/">Template lettre generator</a></h3>
+    <p>Generate a lettre, such as a "lettre de résiliation" in France, rapidly with a given template. </p>
   </div>
 );
